8fe3304fbd7ccb0221419959e0b7c26970690bf1
FlanOfFlans <zty8212000@gmail.com>
2019-02-25 08:32:39 -0600

    Improved messaging from games

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import game
 client = discord.Client()
 prefix = "maf!"
 
-CULL_MESSAGE = ("To save my host's resources, this game has been culled after going on too long. ")
-               ("If this game was still active, and you frequently encounter this problem, please contact the developer. ")
-               ("Currently, games can last for 3 days after being created, or 3 weeks after being started.")
+CULL_MESSAGE = ("To save my host's resources, this game has been culled after going on too long. "
+                "If this game was still active, and you frequently encounter this problem, please contact the developer. "
+                "Currently, games can last for 3 days after being created, or 3 weeks after being started.")
 
 @client.event
 async def on_ready():
@@ -39,10 +39,17 @@ async def tick_games():
       del game.game_dict[game_id]
 
     else:
-      game_output = "\n".join(game.output_buffer)
-      if game_output != "":
-          client.send_message(game.home_channel, game_output)
+      xdict = {}
 
+      for message in game.output_buffer:
+        try:
+          xdict[message[1]].append(message[0])
+        except KeyError:
+          xdict[message[1]] = [message[0]]
+
+      for item in xdict.items():
+        message = "\n".join(item[1])
+        client.send_message(item[0], message)
 
   #loops every minute
   await asyncio.sleep(60)
